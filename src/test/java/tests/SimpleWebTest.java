ac4e622684313eeb03869d0d85c758c5780d59f1
menand <menand@narod.ru>
2025-06-20 21:07:39 +0300

    hw19 done

--- src/test/java/tests/SimpleWebTest.java
+++ src/test/java/tests/SimpleWebTest.java
@@ -4,26 +4,28 @@ import config.WebDriverConfig;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SimpleWebTest extends TestBase {
 
+    private static final Logger log = LoggerFactory.getLogger(SimpleWebTest.class);
+
     @Test
     void githubTitleTest() {
         WebDriverConfig config = ConfigFactory.create(WebDriverConfig.class);
-        WebDriver driver;
         if (config.isRemote()) {
-            System.out.println("Запуск в Selenoid");
+            SimpleWebTest.log.info("Запуск в Selenoid");
             ChromeOptions options = new ChromeOptions();
             options.setBrowserVersion(config.getBrowserVersion());
             driver = new RemoteWebDriver(config.getRemoteUrl(), options);
         } else {
-            System.out.println("Локальный запуск");
+            SimpleWebTest.log.info("Локальный запуск");
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
         }
